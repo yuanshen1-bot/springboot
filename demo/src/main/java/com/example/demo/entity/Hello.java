b09b44629fb5e4ea3b826a74cc12e7db625dda90
jjm <498080458@qq.com>
2023-05-23 16:36:06 +0800

    test

--- demo/src/main/java/com/example/demo/entity/Hello.java
+++ demo/src/main/java/com/example/demo/entity/Hello.java
@@ -6,9 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * <p>
@@ -20,6 +18,8 @@ import lombok.Setter;
  */
 @Getter
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @TableName("sys_hello")
 @ApiModel(value = "Hello对象", description = "")
